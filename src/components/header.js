5d7337b0077476b15bafbd12b13533ef39e931cd
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-08-20 02:24:58 +0900

    ヘッダー微修正

--- src/components/header.js
+++ src/components/header.js
@@ -56,52 +56,54 @@ const Header = ({info, state, func, Styles}) => {
 				{/*** トップリスト -- end -- ***/}
 
 				{/*** ヘッダータブリスト -- start -- ***/}
-					<ul
-						className={`
-							${Styles.tabList}
-							flex-space-around
-							align-items-center
-						`}
-					>
-						{info.map(FWList => {
+					<div className={Styles.tabList}>
+						<ul
+							className={`
+								${Styles.tabListWrap}
+								flex-space-around
+								align-items-center
+							`}
+						>
+							{info.map(FWList => {
 
-							// 選択されているタブの場合
-							if (state.MenuTab === FWList.State) {
-								return (
-									<li
-										id={FWList.State}
-										className={`
-											${Styles.headerTab}
-											${Styles.headerTabSelected}
-											tab-${FWList.State}
-										`}
-										key={`tablist${FWList.State}`}
-										onClick={FWList.Func}
-									>
-										{FWList.FW}
-									</li>
-								);
-							}
+								// 選択されているタブの場合
+								if (state.MenuTab === FWList.State) {
+									return (
+										<li
+											id={FWList.State}
+											className={`
+												${Styles.headerTab}
+												${Styles.headerTabSelected}
+												tab-${FWList.State}
+											`}
+											key={`tablist${FWList.State}`}
+											onClick={FWList.Func}
+										>
+											{FWList.FW}
+										</li>
+									);
+								}
 
-							// 選択されていないタブの場合
-							else {
-								return (
-									<li
-										id={FWList.State}
-										className={`
-											${Styles.headerTab}
-											tab-${FWList.State}
-										`}
-										key={`tablist${FWList.State}`}
-										onClick={FWList.Func}
-									>
-										{FWList.FW}
-									</li>
-								);
-							}
+								// 選択されていないタブの場合
+								else {
+									return (
+										<li
+											id={FWList.State}
+											className={`
+												${Styles.headerTab}
+												tab-${FWList.State}
+											`}
+											key={`tablist${FWList.State}`}
+											onClick={FWList.Func}
+										>
+											{FWList.FW}
+										</li>
+									);
+								}
 
-						})}
-					</ul>
+							})}
+						</ul>
+					</div>
 				{/*** ヘッダータブリスト -- end -- ***/}
 
 			</div>
